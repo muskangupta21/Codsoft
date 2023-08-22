10d1cfe065b25a8e15132750bd158a962351e553
muskangupta21 <142207595+muskangupta21@users.noreply.github.com>
2023-08-22 23:12:49 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,26 +1,41 @@
 import java.util.Scanner;
+
+class Game{
+    public int answer;
+    public void msg(){
+        System.out.println("Think of a number between 1 and 100.\nYou have 5 chance to guess.");
+        answer = (int)(Math.random()*100)+1;
+        for (int i = 1; i <= 5; i++) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Attempt "+ i + "\nEnter your guess number: ");
+            int guess = input.nextInt();
+            if (guess == answer) {
+                System.out.println("Guess is correct.");
+                input.close();
+                return;
+            }
+            else if (guess>answer) {
+                System.out.println("Guess is too high.");
+            }
+            else {
+                System.out.println("Guess is too low.");
+            }
+        }
+        System.out.println("Failed!! The number is "+answer);
+    }
+}
+
 public class NumberGame {
     public static void main(String[] args) {
-        int min = 1;    
-        int max = 100; 
-        int i = 1;      
-        int range = (max - min);
-        int ran = (int) (Math.random() * range) + min;  
-        Scanner guess = new Scanner(System.in);
-        System.out.println("Guess any number between 1 and 100: ");
-        while (i <= 5) {
-            System.out.println("Attempt :" + i);
-            System.out.print("Enter your number: ");
-            int num = guess.nextInt();
-            if (ran > num) {
-                System.out.println("Too low");
-            } else if (ran < num) {
-                System.out.println("Too high");
-            } else {
-                System.out.println("You got it!");
-            }
-            i++;
+        Game game = new Game();
+        game.msg();
+        Scanner num = new Scanner(System.in);
+        System.out.println("Do you want to play the game again?\nPress 1 for Yes\nPress 2 for No.");
+        int in = num.nextInt();
+        if (in == 1) {
+            game.msg();
+        } else {
+            System.out.println("Thanks for playing the game!!");
         }
-        System.out.println("The number is " + ran);
     }
 }
